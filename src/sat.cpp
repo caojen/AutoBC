d41484f7dd675124f011e1e7e3fe83bf403bc19e
曹鉴恩 <caojen@mail2.sysu.edu.cn>
2021-07-31 15:01:20 +0800

    debug: show cmd

--- src/sat.cpp
+++ src/sat.cpp
@@ -49,6 +49,7 @@ namespace ltl {
         // 将filename喂给nuXmv
         std::ostringstream cmd("");
         cmd << "/bin/bash -c '" << this->path << " " << filename << " 2>&1'";
+        std::cout << cmd.str() << std::endl;
         FILE* pipe = popen(cmd.str().c_str(), "r");
         if(!pipe) {
           throw std::runtime_error(strerror(errno));
