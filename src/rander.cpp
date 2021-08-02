5f7ac1b3bfeded94ef7cbf34b21b5e5c9bfb1be3
曹鉴恩 <caojen@mail2.sysu.edu.cn>
2021-08-02 21:18:24 +0800

    make fdepth configurable.

--- src/rander.cpp
+++ src/rander.cpp
@@ -21,10 +21,11 @@ int main(int argc, char** argv) {
 
   std::string   nuXmv(argv[1]);
   unsigned      target = atoi(argv[2]);
-  unsigned      mdepth = atoi(argv[3]);
-  unsigned      stime  = atoi(argv[4]);
-  unsigned      vocabLen = atoi(argv[5]);
-  std::string   ofilename(argv[6]);
+  unsigned      fdepth = atoi(argv[3]);
+  unsigned      mdepth = atoi(argv[4]);
+  unsigned      stime  = atoi(argv[5]);
+  unsigned      vocabLen = atoi(argv[6]);
+  std::string   ofilename(argv[7]);
 
   std::ofstream ofstream;
   ofstream.open(ofilename, std::ios::trunc | std::ios::out);
@@ -38,7 +39,7 @@ int main(int argc, char** argv) {
   unsigned loop = 0;
 
   while(true) {
-    auto res = rander(target, Range(3, mdepth));
+    auto res = rander(target, Range(fdepth, mdepth));
     for(auto& pair: res) {
       ++solved;
       ofstream << pair.first << std::endl;
@@ -68,7 +69,8 @@ void usage(int argc, char** argv) {
   std::cout << "- $1: 指明nuXmv的路径，例如: /usr/local/bin/nuXmv" << std::endl;
   std::cout << "      项目中已经包含了linux版本的nuXmv，存放在./bin中。其他版本请到官网下载" << std::endl;
   std::cout << "- $2: 指明需要生成的LTL公式的数量，例如 10000 代表生成 10000 对" << std::endl;
-  std::cout << "- $3: 指明所生成的LTL公式的最高深度，例如 100" << std::endl;
+  std::cout << "- $3: 指明所生成的LTL公式的最低深度，例如 5" << std::endl;
+  std::cout << "- $3: 指明所生成的LTL公式的最高深度，例如 20" << std::endl;
   std::cout << "- $4: 指明每次搜索时间（秒），例如 20" << std::endl;
   std::cout << "- $5: 指明字典中的变量数。变量从小写b开始，到小写z结束。例如 3 代表可用的变量数为 b, c, d" << std::endl;
   std::cout << "- $6: 指明最终输出的结果存放位置。生成的LTL公式会逐步添加到该文件中，如果该文件已经存在，那么重新创建该文件" << std::endl;
@@ -77,5 +79,4 @@ void usage(int argc, char** argv) {
   std::cout << "- 例子：" << std::endl;
   std::cout << "- " << argv[0] << " /usr/local/bin/nuXmv 10000 100 20 3 ./output.txt" << std::endl;
   std::cout << "- " << argv[0] << " ../bin/nuXmv-Linux-2.0.0 30000 30 15 15 ./output.txt" << std::endl;
-
 }
