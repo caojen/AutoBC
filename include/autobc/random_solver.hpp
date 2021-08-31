0eafc61b193feec5d558f6847ebec3152f3d25e9
caojen <caojen@mail2.sysu.edu.cn>
2021-08-31 10:32:19 +0000

    remove true

--- include/autobc/random_solver.hpp
+++ include/autobc/random_solver.hpp
@@ -11,10 +11,14 @@ namespace autobc {
       RandomSolver(const std::set<ltl::LTL>& domains, const ltl::LTL& goal, const Lasso& bc, const std::set<ltl::LTL>& old_goals);
       const std::set<ltl::LTL>& fix_with_limit(unsigned limit);
 
+    private:
+
       static std::set<ltl::LTL> RS(const ltl::LTL& formula);
       bool repair_success(const ltl::LTL& formula);
 
-    private:
+      static ltl::LTL randOp1(const ltl::LTL& f, std::shared_ptr<ltl::Operator*> op);
+      static ltl::LTL randOp2(const ltl::LTL& f1, std::shared_ptr<ltl::Operator*> op, const ltl::LTL f2);
+
       std::set<ltl::LTL>      domains;
       ltl::LTL                goal;
       Lasso                   bc;
