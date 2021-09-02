d1c3953e108629cc7c7117d92cdc25647d3813a6
caojen <caojen@mail2.sysu.edu.cn>
2021-09-02 05:40:47 +0000

    remove logs

--- src/autobc/ranking.cpp
+++ src/autobc/ranking.cpp
@@ -84,7 +84,6 @@ namespace autobc {
       ltls.insert(target);
       denominator_a = this->mc->count(ltls, this->bound);
     }
-    std::cout << "denominator_a = " << denominator_a << std::endl;
 
     for(auto& replacement: replacements) {
       auto end = std::chrono::system_clock::now();
