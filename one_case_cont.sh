b3851557606a86ebf565dd746059027d787da31c
caojen <caojen@mail2.sysu.edu.cn>
2021-09-03 18:19:30 +0000

    update

--- one_case_cont.sh
+++ one_case_cont.sh
@@ -14,28 +14,32 @@ ranking_bin="$(pwd)/build/ranking"
 ref_result_file="${target_dir}/ref_result.txt"
 
 if [ ! -f ${ref_result_file} ];then
+  echo "ref_result : $ref_result_file"
   ${ref_bin} --global ${domainfile} --bcfile ${bcfile} -8 /usr/bin/java -6 /jdk-16/bin/java -k 100 -t 100 --likelyhood /src/autobc/Lasso-BC/likelyhood.jar --modelcounting /src/autobc/jar/modelcounting.jar --nuxmv /src/autobc/bin/nuXmv-2.0.0-Linux --output ${ref_result_file}
   sleep 1s
 fi
 
-random_result_file="${workspace}/random_result.txt"
+random_result_file="${target_dir}/random_result.txt"
 
 if [ ! -f ${random_result_file} ];then
+  echo "random_result: ${random_result_file}"
   ${random_bin} --global ${domainfile} --bcfile ${bcfile} -8 /usr/bin/java -6 /jdk-16/bin/java -k 100 -t 100 --likelyhood /src/autobc/Lasso-BC/likelyhood.jar --modelcounting /src/autobc/jar/modelcounting.jar --nuxmv /src/autobc/bin/nuXmv-2.0.0-Linux --output ${random_result_file}
   sleep 1s
 fi
 
 # 排序，生成普通文件 rank_result
   
-rank_result_file="${workspace}/rank_result.txt"
+rank_result_file="${target_dir}/rank_result.txt"
 if [ ! -f ${rank_result_file} ];then
+  echo "rank_result: ${rank_result_file}"
   ${ranking_bin} -8 /usr/bin/java -6 /jdk-16/bin/java -k 100 -t 100 --likelyhood /src/autobc/Lasso-BC/likelyhood.jar --modelcounting /src/autobc/jar/modelcounting.jar -n /src/autobc/bin/nuXmv-2.0.0-Linux -f ${ref_result_file} -r ${random_result_file} | tee ${rank_result_file}
   sleep 1s
 fi
 
 # 排序，生成普通文件 rank_m_result
-rank_m_result_file="${workspace}/rank_m_result.txt"
+rank_m_result_file="${target_dir}/rank_m_result.txt"
 if [ ! -f ${rank_m_result_file} ];then
+  echo "rank_m_result_file: ${rank_m_result_file}"
   ${ranking_bin} -8 /usr/bin/java -6 /jdk-16/bin/java -k 100 -t 100 --likelyhood /src/autobc/Lasso-BC/likelyhood.jar --modelcounting /src/autobc/jar/modelcounting.jar -n /src/autobc/bin/nuXmv-2.0.0-Linux -f ${ref_result_file} -r ${random_result_file} --model | tee ${rank_m_result_file}
   sleep 1s
 fi
